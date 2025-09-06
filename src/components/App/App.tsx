5987e24654ac0c3998366a68a2952df2640a2d56
Alexsandr Kharchenko <googfather8@gmail.com>
2025-09-06 15:47:57 +0300

    project into production

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,11 @@
 import { useCallback, useState } from 'react';
 import { Toaster, toast } from 'react-hot-toast';
-import SearchBar from './components/SearchBar/SearchBar';
-import MovieGrid from './components/MovieGrid/MovieGrid';
-import Loader from './components/Loader/Loader';
-import ErrorMessage from './components/ErrorMessage/ErrorMessage';
-import { fetchMovies } from './services/movieService';
-import type { Movie } from './types/movie';
+import SearchBar from '../SearchBar/SearchBar';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import { fetchMovies } from '../../services/movieService';
+import type { Movie } from '../../types/movie';
 
 export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
@@ -15,7 +15,7 @@ export default function App() {
 
   const handleSearch = useCallback(async (query: string) => {
     setError(null);
-    setMovies([]); // очищення попереднього пошуку
+    setMovies([]);
     setLoading(true);
 
     try {
@@ -44,28 +44,25 @@ export default function App() {
         <MovieGrid movies={movies} onSelect={m => setSelected(m)} />
       )}
 
-      {/* Модалка підключається тут, але компонент створює портал сам */}
-      {/* Щоб прибрати вибраний фільм після закриття, передаємо onClose */}
+      {}
+      {}
       {selected && (
         <div style={{ display: 'contents' }}>
-          {/* Ледачий імпорт не потрібен, бо застосунок невеликий */}
-          {/*
-            В окремий компонент винесено логіку й портал.
-            Тут важливо НІЧОГО не ламати в дереві, тому wrapper = display: contents
-          */}
+          {}
+          {}
         </div>
       )}
-      {/* Безпосередньо імпортуємо MovieModal, щоб TS знав типи */}
-      {/* І рендеримо його умовно внизу: */}
-      {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-      {/* @ts-ignore */}
+      {}
+      {}
+      {}
+      {}
       <MovieModalContainer movie={selected} onClose={() => setSelected(null)} />
     </>
   );
 }
 
 // Окремий контейнер, щоб уникнути умовного імпорту зверху та мати чистий App
-import MovieModal from './components/MovieModal/MovieModal';
+import MovieModal from '../MovieModal/MovieModal';
 interface MovieModalContainerProps {
   movie: Movie | null;
   onClose: () => void;
